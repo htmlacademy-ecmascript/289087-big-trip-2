73c74a0c121f0bfbd983c4a60546b8a10691a95d
aliagiray <alia.giray@icloud.com>
2026-08-12 07:58:39 +0200

    исправляет замечания по ревью

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import OffersApiService from './api-service/offers-api-service.js';
 import TripInfoPresenter from './presenter/trip-info-presenter.js';
 
 const AUTHORIZATION_BASE = 36;
+const AUTHORIZATION_START_INDEX = 2;
 const AUTHORIZATION = `Basic ${Math.random().toString(AUTHORIZATION_BASE).slice(AUTHORIZATION_START_INDEX)}`;
 const API_ENDPOINT = 'https://22.objects.htmlacademy.pro/big-trip';
 
